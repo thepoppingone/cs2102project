4f4ed8a3b2d32ff7368e3e4f159d1092653b6852
Goh Xue Li <A0098969@nus.edu.sg>
2015-03-13 13:53:44 +0800

    - admin can now login and will be redirected to the admin_panel.html - check the sql files for any admin account info

--- cs2102-files/html/user_checklogin.php
+++ cs2102-files/html/user_checklogin.php
@@ -1,5 +1,30 @@
 <?php
-if(!empty($_POST)){ 
-	header("location:admin_panel.html");
+	
+if(!empty($_POST)){
+
+	require("config.php");
+	
+	// check login credentials for admin
+	$email = $_POST['email'];
+	$pwd = $_POST['password'];
+	
+	// carry out sql command
+	$sql = "SELECT * FROM admin a WHERE a.email = '".$email."' AND a.password = '".$pwd."'";
+
+	$stid = oci_parse($dbh, $sql);
+
+	// without OCI_DEFAULT any changes to the database will be instantly viewable by all other connecgtions
+	oci_execute($stid, OCI_DEFAULT); 
+
+	if ($row = oci_fetch_array($stid)) 
+	{
+		header("location:admin_panel.html");
+		
+	} else {
+		header("location:user_login.php");
+	}
+
+	// to free up the resources
+	oci_free_statement($stid);
 }
 ?>
