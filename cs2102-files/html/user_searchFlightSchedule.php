8e45489b67b740436658245e4479990dd5fdaa2c
thepoppingone <admin@Poh-Pengs-Macbook-Air.local>
2015-03-15 22:03:18 +0800

    FINALLY, the search for flights are now available!!! Do update me what information should be provided in the results! notes: default way to add a day to a TO_TIMESTAMP(date, "format") is just at a +1 to the end of the statement!

--- cs2102-files/html/user_searchFlightSchedule.php
+++ cs2102-files/html/user_searchFlightSchedule.php
@@ -20,36 +20,31 @@ if(!empty($_GET)){
 		// carry out sql command
 		$sql = "SELECT * FROM schedule s, flight f 
 				WHERE s.depart_time >= TO_TIMESTAMP('".$departure_date."', 'YYYY-MM-DD')
+				AND s.depart_time < TO_TIMESTAMP('".$departure_date."', 'YYYY-MM-DD')+1
 				AND f.origin LIKE '%".$origin."%'
 				AND f.destination LIKE '%".$destination."%' 
 				AND f.f_number = s.flight_number 
 				AND f.designator = s.designator";
+				
 
 		$stid = oci_parse($dbh, $sql);
 
 		// without OCI_DEFAULT any changes to the database will be instantly viewable by all other connecgtions
 		oci_execute($stid, OCI_DEFAULT); 
-
-     if(empty($rows)) {
-		echo "<tr>";
-			echo "<td colspan='4'>There were not records</td>";
-		echo "</tr>";
-	}
-	else {
-		foreach ($rows as $row) {
+		
+		//FETCH AN ASSOICATIVE ARRAY FOR DATA EXTRACTION
+		while ($row = oci_fetch_assoc($stid)) 
+		{
+			//NOTE THE ASSOCIATIVE ARRAY REACTS ONLY TO CAPITAL LETTERS
 			echo "<tr>";
-				echo "<td>".$row['designator']."</td>";
-				echo "<td>".$row['flight_number']."</td>";
-				echo "<td>".$row['departure_time']."</td>";
-				echo "<td>".$row['price']."</td>";
+				echo "<td>".$row['DESIGNATOR']."</td>";
+				echo "<td>".$row['FLIGHT_NUMBER']."</td>";
+				echo "<td>".$row['DEPART_TIME']."</td>";
+				echo "<td>".$row['PRICE']."</td>";
 			echo "</tr>";
 		}
-	}
-
-		while ($row = oci_fetch_array($stid)) 
-		{
-			echo $row[0];
-		}
+	
+	
 		
 		echo "end of file";
 		
