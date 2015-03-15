3230b6811217515c864d386417b0c8a0fc2d6985
Lee Cheryl <snowylee_36@hotmail.com>
2015-03-15 13:04:21 +0800

    added excel which i referred to for airport details, added airport.sql

---
+++ cs2102-files/sql/airport.sql
@@ -0,0 +1,40 @@
+CREATE TABLE airport (
+name VARCHAR(256),
+location VARCHAR(256),
+designator VARCHAR(10) PRIMARY KEY
+);
+
+INSERT INTO airport VALUES('108 Mile Ranch', '108 Mile Ranch, Canada', 'ZMH');
+INSERT INTO airport VALUES('Aachen/Merzbruck', 'Aachen, Germany', 'AAH');
+INSERT INTO airport VALUES('Railway', 'Aachen, Germany', 'ZIU');
+INSERT INTO airport VALUES('Aalborg', 'Aalborg, Denmark', 'AAL');
+INSERT INTO airport VALUES('Vigra', 'Aalesund, Norway', 'AES');
+INSERT INTO airport VALUES('Tirstrup', 'Aarhus, Denmark', 'AAR');
+INSERT INTO airport VALUES('Aarhus Limo', 'Aarhus Limo, Denmark', 'ZBU');
+INSERT INTO airport VALUES('Aasiaat', 'Aasiaat, Greenland', 'JEG');
+INSERT INTO airport VALUES('Abadan', 'Abadan, Iran', 'ABD');
+INSERT INTO airport VALUES('Abaiang', 'Abaiang, Kiribati', 'ABF');
+INSERT INTO airport VALUES('Abakan', 'Abakan, Russian Federation', 'ABA');
+INSERT INTO airport VALUES('Abau', 'Abau, Papua New Guinea', 'ABW');
+INSERT INTO airport VALUES('Abbotsford', 'Abbotsford, Canada', 'YXX');
+INSERT INTO airport VALUES('Mumbai', 'Abbottabad, Pakistan', 'AAW');
+INSERT INTO airport VALUES('Abbse', 'Abbse, Yemen', 'EAB');
+INSERT INTO airport VALUES('Abecher', 'Abecher, Chad', 'AEH');
+INSERT INTO airport VALUES('Abemama Atoll', 'Abemama Atoll, Kiribati', 'AEA');
+INSERT INTO airport VALUES('Abengourou', 'Abengourou, Ivory Coast', 'OGO');
+INSERT INTO airport VALUES('Dyce', 'Aberdeen, United Kingdom', 'ABZ');
+INSERT INTO airport VALUES('Phillips AAF', 'Aberdeen, United States', 'APG');
+INSERT INTO airport VALUES('Municipal', 'Aberdeen, United States', 'ABR');
+INSERT INTO airport VALUES('Abha', 'Abha, Saudi Arabia', 'AHB');
+INSERT INTO airport VALUES('Felix Houphouet Boigny', 'Abidjan, Ivory Coast', 'ABJ');
+INSERT INTO airport VALUES('Dyess AFB', 'Abilene, United States', 'DYS');
+INSERT INTO airport VALUES('Municipal', 'Abilene, United States', 'ABI');
+INSERT INTO airport VALUES('RAF Station', 'Abingdon, United Kingdom', 'ABB');
+INSERT INTO airport VALUES('Abingdon', 'Abingdon, Australia', 'ABG');
+INSERT INTO airport VALUES('Virginia Highlands', 'Abingdon, United States', 'VJI');
+INSERT INTO airport VALUES('Aboisso', 'Aboisso, Ivory Coast', 'ABO');
+INSERT INTO airport VALUES('Abou Deia', 'Abou Deia, Chad', 'AOD');
+INSERT INTO airport VALUES('Abreojos', 'Abreojos, Mexico', 'AJS');
+
+--stopped at line 65		
+
