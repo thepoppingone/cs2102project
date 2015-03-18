08d54749cfe0deae0a787b0a9bb697b1a6b66d80
kjiale <kwang_jiale@hotmail.com>
2015-03-19 03:30:16 +0800

    edited the attribute for add flight and add schedule

--- cs2102-files/html/admin_func_add_flight.php
+++ cs2102-files/html/admin_func_add_flight.php
@@ -1,28 +1,27 @@
 <?php 
 	
-	$designator = $_POST['designator'];
-	$f_number = $_POST['number'];
-	$origin = $_POST['origin'];
-	$destination = $_POST['destination'];
+	$f_number = $_POST['f_number'];
 	$duration = $_POST['duration'];
+	$destination = $_POST['destination'];
+	$origin = $_POST['origin'];	
+	$seat_capacity = $_POST['seat_capacity'];		
 	
 	require("config.php");
-	$sql = "SELECT * FROM flight f WHERE f.designator = '".$designator."'
-			AND f.f_number='".$f_number."'";
+	$sql = "SELECT * FROM flight f WHERE f.f_number='".$f_number."'";
 	$stid = oci_parse($dbh, $sql);
 	oci_execute($stid, OCI_DEFAULT);
 	if($row = oci_fetch_array($stid)) {
 		echo "flight_exists";
 	} else {
 		// insert the record in
-		$sql = "INSERT INTO flight VALUES(:f_number, :duration, :destination, :origin, :designator)";
+		$sql = "INSERT INTO flight VALUES(:f_number, :duration, :destination, :origin, :seat_capacity)";
 		
 		$stid = oci_parse($dbh, $sql);
 		oci_bind_by_name($stid, ':f_number', $f_number);
 		oci_bind_by_name($stid, ':duration', $duration);
 		oci_bind_by_name($stid, ':destination', $destination);
 		oci_bind_by_name($stid, ':origin', $origin);
-		oci_bind_by_name($stid, ':designator', $designator);
+		oci_bind_by_name($stid, ':seat_capacity', $seat_capacity);
 		
 		$result = oci_execute($stid);
 		if(!$result) {
--- cs2102-files/html/admin_func_add_schedule.php
+++ cs2102-files/html/admin_func_add_schedule.php
@@ -1,18 +1,17 @@
 <?php 
 	
 	date_default_timezone_set('Asia/Singapore');
-	
-	$designator = $_POST['designator'];
-	$f_number = $_POST['f_number'];
-	$aircraft = $_POST['aircraft'];
-	$seatNum = $_POST['seatNum'];
-	$price = $_POST['price'];
-	$arrival = $_POST['arrival'];
-	$departure =  $_POST['departure'];
+
+	$arrival_time = $_POST['arrival_time'];	
+	$depart_time = $_POST['depart_time'];
+	$num_of_seats_avail = $_POST['num_of_seats_avail'];
+	$price = $_POST['price'];			
+	$flight_number = $_POST['flight_number'];
+
 	
 	require("config.php");
-	$sql = "SELECT * FROM schedule s WHERE s.flight_number = '".$f_number."'
-			 AND s.depart_time = TO_TIMESTAMP('".$departure."', 'YYYY-MM-DD\"T\"HH24:MI:SS')";
+	$sql = "SELECT * FROM schedule s WHERE s.flight_number = '".$flight_number."'
+			 AND s.depart_time = TO_TIMESTAMP('".$depart_time."', 'YYYY-MM-DD\"T\"HH24:MI:SS')";
 	$stid = oci_parse($dbh, $sql);
 	oci_execute($stid, OCI_DEFAULT);
 	if($row = oci_fetch_array($stid)) {
@@ -24,13 +23,12 @@
 		TO_TIMESTAMP('1970-01-01T07:30:00', 'YYYY-MM-DD"T"HH24:MI:SS')
 		*/
 		$stid = oci_parse($dbh, $sql);
-		oci_bind_by_name($stid, ':arrival', $arrival);
-		oci_bind_by_name($stid, ':departure', $departure);
-		oci_bind_by_name($stid, ':seatNum', $seatNum);
+		oci_bind_by_name($stid, ':arrival_time', $arrival_time);
+		oci_bind_by_name($stid, ':depart_time', $depart_time);
+		oci_bind_by_name($stid, ':num_of_seats_avail', $num_of_seats_avail);
 		oci_bind_by_name($stid, ':price', $price);
-		oci_bind_by_name($stid, ':designator', $designator);
-		oci_bind_by_name($stid, ':f_number', $f_number);
-		oci_bind_by_name($stid, ':aircraft', $aircraft);
+		oci_bind_by_name($stid, ':flight_number', $flight_number);
+
 		
 		$result = oci_execute($stid);
 		if(!$result) {
