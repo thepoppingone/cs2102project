af8d8e6284cb18c9ea266eaa9902d52fb8514846
thepoppingone <admin@Poh-Pengs-Macbook-Air.local>
2015-03-23 03:21:55 +0800

    remove js codes

--- cs2102-files/html/user_passengers.php
+++ cs2102-files/html/user_passengers.php
@@ -126,23 +126,11 @@ echo "Num of children: ". $_SESSION['child']."<br/>";
 ?>
 
     </div>
- 
 
     <script type="text/javascript">
 
 // Read a page's GET URL variables and return them as an associative array.
-function getUrlVars()
-{
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
-}
+
 // the above function is to make it easy to retrieve values from GET
 
 
--- cs2102-files/html/user_search_results.php
+++ cs2102-files/html/user_search_results.php
@@ -92,22 +92,6 @@
  
 
     <script type="text/javascript">
-//use jquery assign each SELECT button by ID value
-
-// Read a page's GET URL variables and return them as an associative array.
-function getUrlVars()
-{
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
-}
-// the above function is to make it easy to retrieve values from GET
 
     window.onload = function() {
  makeAjaxRequest();
