fbc471645777f4e4199137169856b2fcffeff6c5
thepoppingone <admin@Poh-Pengs-Macbook-Air.local>
2015-04-01 13:35:49 +0800

    Updated Schema to include DEFERRABLE constraints plus ON DELETE CASCADE

--- cs2102-files/sql/admin.sql
+++ cs2102-files/sql/admin.sql
@@ -3,3 +3,19 @@ INSERT INTO admin VALUES('Qian Jun Yip', 'qj.en.yip@gmail.com', 'junyip234');
 INSERT INTO admin VALUES('Lee Cheryl', 'leecheryl993@gmail.com', 'chery345');
 INSERT INTO admin VALUES('Jia Le Kwang', 'kwangjiale@gmail.com', 'jiale456');
 INSERT INTO admin VALUES('Goh Xue Li', 'gohxueli.gxl@gmail.com', 'xueli567');
+
+
+--SELECT * FROM Schedule
+--WHERE flight_number = 'SB2222'; 
+
+--UPDATE Schedule s
+--SET s.flight_number = 'SB2222'
+--WHERE s.flight_number = 'SB222';
+
+--UPDATE Booking b
+--SET b.flight_number = 'SB2222'
+--WHERE b.flight_number = 'SB222';
+
+
+--SELECT * FROM Booking
+--WHERE flight_number = 'SB2222'; 
